9b00e91ca51e2b20916729d03c1970470fb7028d
areph <40balmung@gmail.com>
2018-08-12 19:21:54 +0900

    change views directory to app directory

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ let usersRouter = require('./routes/users');
 let app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app/views'));
 app.set('view engine', 'twig');
 
 app.use(logger('dev'));
